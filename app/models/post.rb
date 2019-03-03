14e316bc9730aafd8c858766bc4ab6882594e986
hongsondog <ungthienthanh97@gmail.com>
2019-03-04 01:50:12 +0700

    initial project

---
+++ app/models/post.rb
@@ -0,0 +1,43 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id               :integer          not null, primary key
+#  title            :string
+#  body             :text
+#  description      :text
+#  slug             :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  banner_image_url :string
+#  author_id        :integer
+#  published        :boolean          default(FALSE)
+#  published_at     :datetime
+#
+
+class Post < ApplicationRecord
+	extend FriendlyId
+	friendly_id :title, use: :slugged
+	belongs_to :author, :optional => true
+	# <!-- sap xep bai dang theo thu tu moi nhat-->
+	scope :most_recent, -> { order(published_at: :desc) }
+	scope :published, -> { where(published: true) }
+	def should_generate_new_friendly_id?
+		title_changed?
+	end
+	# <!-- ham lay thoi gian-->
+	def display_day_published 
+		if published_at.present?
+		"Published #{published_at.strftime('%-b %-d, %Y')}"
+	else
+		"not published"
+	end
+end
+	def publish
+		update(published: true, published_at: Time.now)
+	end
+	def unpublish
+		update(published: false, published_at: nil)
+	end
+end
+ 
